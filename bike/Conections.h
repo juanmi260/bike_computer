5a940130e2696fdf205fd42906eae2d2557a8c71
Juan Miguel <juanmi260@gmail.com>
2019-10-23 21:29:30 +0200

    Added TFT support to display data

---
+++ bike/Conections.h
@@ -0,0 +1,16 @@
+// The ESP32 and TFT the pins used for testing are:
+#define TFT_CS   33  // Chip select control pin (library pulls permanently low
+#define TFT_DC   15  // Data Command control pin - must use a pin in the range 0-31
+#define TFT_RST  32  // Reset pin, toggles on startup
+
+#define TFT_WR    4  // Write strobe control pin - must use a pin in the range 0-31
+#define TFT_RD    2  // Read strobe control pin
+
+#define TFT_D0   12  // Must use pins in the range 0-31 for the data bus
+#define TFT_D1   13  // so a single register write sets/clears all bits.
+#define TFT_D2   26  // Pins can be randomly assigned, this does not affect
+#define TFT_D3   25  // TFT screen update performance.
+#define TFT_D4   17
+#define TFT_D5   16
+#define TFT_D6   27
+#define TFT_D7   14
--- bike/bike.ino
+++ bike/bike.ino
@@ -1,36 +1,71 @@
+#include "Conections.h"
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <Adafruit_GFX.h>
+#include <MCUFRIEND_kbv.h>
+#include <FreeDefaultFonts.h>
+#include "Fonts/FreeSans9pt7b.h"
+#include "Fonts/FreeSans12pt7b.h"
+#include "Fonts/FreeSerif12pt7b.h"
 
 // Firmaware
 #define DEBUG
 
-// BME vars
-//SCL A5
-//SDA A4
+// BME values
+//SCL_BME A5 21(esp32)
+//SDA_BME A4 22(esp32)
 Adafruit_BME280 bme; // I2C
 #define SEALEVELPRESSURE_HPA (1013.25)
 #define BME_TIMER 1000
 float temperature = 0, pressure = 0, altitude = 0, humidity = 0;
 unsigned long lastMillisBME = 0;
 
-// GPS vars
-#define GPS_RX 3
-#define GPS_TX 4
+// GPS values
+#define GPS_RX 34
+#define GPS_TX 35
 #define GPS_BAUD 9600
 #define GPS_TIMER 1000
 #define MIN_SATELLITES 5
 #define GPS_DELAY 500
 TinyGPSPlus gps;
-SoftwareSerial ss(GPS_TX, GPS_RX);
-int satellites = 0, fix = 0, age = 0;
+SoftwareSerial ss;
+int satellites = -1, fix = 0, age = 0;
 unsigned long lastMillisGPS = 0;
 float latitude = 0, longitude = 0, speed = 0, hdop = 0;
 TinyGPSDate date;
-TinyGPSTime time;
+TinyGPSTime timegps;
+
+// TFT-LCD-TouchScreen values
+MCUFRIEND_kbv tft;
+
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+#define RGB(r, g, b) (((r&0xF8)<<8)|((g&0xFC)<<3)|(b>>3))
+
+#define GREY      RGB(127, 127, 127)
+#define DARKGREY  RGB(64, 64, 64)
+#define TURQUOISE RGB(0, 128, 128)
+#define PINK      RGB(255, 128, 192)
+#define OLIVE     RGB(128, 128, 0)
+#define PURPLE    RGB(128, 0, 128)
+#define AZURE     RGB(0, 128, 255)
+#define ORANGE    RGB(255,128,64)
+
+#define TFT_TIMER 1000
+unsigned long lastMillisTFT = 0;
+
+
 
 // Print vars
 unsigned long lastMillisOUT = 0;
@@ -57,18 +92,39 @@ void setup() {
 
     Serial.println("Starting gps tracker");
 
+    tftSetup();
+    showSpashScreen();
+
+    //Setup sensors
     bmeSetup();
     gpsSetup();
+    
+    tft.fillScreen(BLACK);
 }
 
 
 void loop() {     
     readBME();
     gpsRead();
+    int menu = 0;
+    switch(menu) {
+      case 0:
+          refreshScreen();
+          break;
+      case 1:
+        refreshStatusScreen();
+        break;
+      case 2:
+        refreshScreen();
+        break;
+    }
+    
+    refreshScreen();
+    //printValues();
     #ifdef DEBUG
       //printValues();
     #endif
-      logParameters();
+      //logParameters();
 }
 
 /**
@@ -115,7 +171,7 @@ void readBME()
 void gpsSetup()
 {
   Serial.println("GPS setup....: inicializando");
-  ss.begin(GPS_BAUD);
+  ss.begin(GPS_BAUD, GPS_RX, GPS_TX);
   Serial.println("GPS setup....: ok");
 }
 
@@ -136,8 +192,11 @@ void gpsRead()
     longitude = gps.location.lng();
     speed = gps.speed.kmph();
     date = gps.date;
-    time = gps.time;
+    timegps = gps.time;
     fix = gps.sentencesWithFix();
+
+    Serial.print("GPS satellites: ");
+    Serial.println(satellites);
 }
 
 /**
@@ -191,9 +250,9 @@ void logParameters()
             date.year(),
             date.month(),                            
             date.day(),
-            time.hour(),
-            time.minute(),
-            time.second(),
+            timegps.hour(),
+            timegps.minute(),
+            timegps.second(),
             fix,
             satellites,
             dtostrf(hdop, 1, 6, data[3]),
@@ -204,4 +263,70 @@ void logParameters()
         );
         Serial.println(buffer);
     }
+}
+
+/**
+ * TFT setup
+ */
+void tftSetup() {
+    Serial.println("TFT setup....: inicializando");
+    uint16_t ID;
+    tft.reset();
+    ID = tft.readID();
+    Serial.print("TFT ID = 0x");
+    Serial.println(ID, HEX);
+    //    if (ID == 0xD3D3) ID = 0x9481; // write-only shield
+    if (ID == 0xD3D3) ID = 0x9486; // write-only shield
+    tft.begin(ID);
+    tft.setRotation(0);
+    tft.fillScreen(BLACK);
+    showmsgXY(5, 180, 1, &FreeSevenSegNumFont, "Hola");
+    Serial.println("GPS setup....: ok");
+}
+
+/**
+ * Print message in LCD
+ */
+void showmsgXY(int x, int y, int sz, const GFXfont *f, const char *msg)
+{
+    int16_t x1, y1;
+    uint16_t wid, ht;
+    //tft.drawFastHLine(0, y, tft.width(), RED);
+    tft.setFont(f);
+    tft.setCursor(x, y);
+    tft.setTextColor(WHITE,RED);
+    tft.setTextSize(sz);
+    tft.print(msg);
+    delay(1000);
+}
+
+/**
+ * Load splash screen while setting up other sensors
+ */
+void showSpashScreen()
+{
+  showmsgXY(20, 200, 1, &FreeSans12pt7b, "Opensource Bike Computer");
+  showmsgXY(130, 220, 1, &FreeSans9pt7b, "Rev1.0");
+  showmsgXY(90, 240, 1, &FreeSans9pt7b, "By Juanmi_260");
+  delay(1000);
+}
+
+void refreshScreen()
+{
+  if (millis() - lastMillisTFT >= TFT_TIMER) {
+    char buffer[20];
+    char tmp[20];
+    lastMillisTFT = millis();
+    sprintf(buffer, "Temperature: %s", dtostrf(temperature, 3, 2, tmp));
+    showmsgXY(20, 200, 1, &FreeSans12pt7b, buffer);
+    /*char lat[15], lng[15], spd[15];
+    char buffer[200];
+    sprintf(buffer, "Satelites: %i Latitud: %s Longitud: %s Velocidad: %s", satellites, dtostrf(latitude, 3, 7, lat), dtostrf(longitude, 3, 7, lng), dtostrf(latitude, 3, 7, spd));
+    Serial.println(buffer);*/
+  }
+}
+
+void refreshStatusScreen()
+{
+  tft.fillScreen(RED);
 }
